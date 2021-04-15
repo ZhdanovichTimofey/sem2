f428090d19a033c918ae87ea915e6eace601240c
ZhdanovichTimofey <71005308+ZhdanovichTimofey@users.noreply.github.com>
2021-04-15 11:57:43 +0300

    second

--- TEST_1/3.cpp
+++ TEST_1/3.cpp
@@ -2,7 +2,7 @@
 
 void sort(int* first, int* last){
 	int n = last - first;
-	int o = first[n/2 - 1], k = 0, f = 0;
+	int k = 0, f = 0;
 	for (int i = 0; i < n/2 + 1; ++i){
 		for (int j = f; j < n/2; ++j){
 			if (first[i] >= first[n - 1 - j]){
